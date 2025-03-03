45adf72cb0b0763843b6c4d56eed0b71acccdef4
Abdullah Azeez <PantheraUnemotional@gmail.com>
2025-03-03 23:08:55 +0800

    implement spring boot

---
+++ backend/src/main/java/com/campus/SmartCampusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusBackendApplication.class, args);
+	}
+
+}
