60fe4a3a25de3984af2692c8a70f88d1289b6e23
yqn1e23 <youquanneu3@gmail.com>
2025-03-07 18:46:52 +0800

    Some features adjustment

---
+++ backend/src/main/java/com/campus/Controller/ContentController.java
@@ -0,0 +1,16 @@
+package com.campus.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContentController {
+//    @GetMapping("/test")
+//    public String homePage() {
+//        return "home";
+//    }
+//    @GetMapping("/admin/home")
+//    public String adminHomePage(){
+//        return "adminHomePage";
+//    }
+}
