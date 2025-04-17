7dfb255da929ecbe0c826191a3403bbd84cd0510
yqn1e23 <youquanneu3@gmail.com>
2025-04-17 21:51:38 +0800

    .

--- backend/src/main/java/com/campus/Controller/User/AdminController.java
+++ backend/src/main/java/com/campus/Controller/User/AdminController.java
@@ -2,6 +2,7 @@ package com.campus.Controller.User;
 
 import com.campus.Entity.User.User;
 import com.campus.Service.User.AdministrativeStaffService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import java.util.logging.Logger;
 @RequestMapping("/admin")
 public class AdminController {
     private static final Logger logger = Logger.getLogger(AdminController.class.getName());
+    @Autowired
     private AdministrativeStaffService administrativeStaffService;
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
